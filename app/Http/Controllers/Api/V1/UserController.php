2088315a5c2773dd1847c3c25a52f34921638125
谢榕川 <temurumaru@yandex.com>
2025-05-02 02:40:52 +0800

    Update self method in UserController to return JsonResponse instead of User

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Js;
 
 class UserController extends Controller
 {
-    public function self(Request $request): User
+    public function self(Request $request): JsonResponse
     {
-        return $request->user();
+        return response()->json($request->user());
     }
 }
