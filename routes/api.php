3a00e0dabb2d98723cdb00bb0442d82ec0daef3b
谢榕川 <temurumaru@yandex.com>
2025-05-01 22:34:51 +0800

    Add API routes for token creation and user authentication

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,33 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
+use App\Models\User;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Cache;
 
 Route::get('/', function () {
     return "OK 200";
+});
+
+Route::get('/set/{key}/{data}', function ($key, $data) {
+    Cache::put($key, $data, 30); // 600 секунд = 10 минут
+    return "Data cached: $data";
+});
+
+Route::get('/get/{key}', function ($key) {
+    $data = Cache::get($key, 'not found');
+    return "Cached data: $data";
 });
