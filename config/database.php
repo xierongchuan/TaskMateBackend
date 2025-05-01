9e8a020a3af4a44b76d33f5a25a522e34c211eca
谢榕川 <temurumaru@yandex.com>
2025-05-01 22:33:33 +0800

    Refactor Redis configuration to standardize environment variable names for host, password, and port

--- config/database.php
+++ config/database.php
@@ -153,19 +153,19 @@ return [
 
         'default' => [
             'url' => env('REDIS_URL'),
-            'host' => env('VALKEY_HOST', '127.0.0.1'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
-            'password' => env('VALKEY_PASSWORD'),
-            'port' => env('VALKEY_PORT', '6379'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
         ],
 
         'cache' => [
             'url' => env('REDIS_URL'),
-            'host' => env('VALKEY_HOST', '127.0.0.1'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
-            'password' => env('VALKEY_PASSWORD'),
-            'port' => env('VALKEY_PORT', '6379'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
 
