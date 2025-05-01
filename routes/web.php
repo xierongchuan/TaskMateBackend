cbba3dfb9e02b1f5069ef7b14f39f6af3e6524cb
谢榕川 <temurumaru@yandex.com>
2025-05-01 23:11:06 +0800

    Comment out cache-related routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,12 @@ Route::get('/', function () {
     return "OK 200";
 });
 
-Route::get('/set/{key}/{data}', function ($key, $data) {
-    Cache::put($key, $data, 30); // 600 секунд = 10 минут
-    return "Data cached: $data";
-});
+// Route::get('/set/{key}/{data}', function ($key, $data) {
+//     Cache::put($key, $data, 30); // 600 секунд = 10 минут
+//     return "Data cached: $data";
+// });
 
-Route::get('/get/{key}', function ($key) {
-    $data = Cache::get($key, 'not found');
-    return "Cached data: $data";
-});
+// Route::get('/get/{key}', function ($key) {
+//     $data = Cache::get($key, 'not found');
+//     return "Cached data: $data";
+// });
