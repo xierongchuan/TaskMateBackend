eb3933c3fd80addcae35a58a2efe21a55fb769eb
谢榕川 <xierongchuan@tutamail.com>
2025-07-27 02:22:52 +0500

    Correct comment

--- routes/telegram.php
+++ routes/telegram.php
@@ -5,13 +5,7 @@
 use SergiX44\Nutgram\Nutgram;
 
 /*
-|--------------------------------------------------------------------------
 | Nutgram Handlers
-|--------------------------------------------------------------------------
-|
-| Here is where you can register telegram handlers for Nutgram. These
-| handlers are loaded by the NutgramServiceProvider. Enjoy!
-|
 */
 
 $bot->onCommand('start', function (Nutgram $bot) {
