4cf3da251ed86fb6326237ae76261db171bbab65
谢榕川 <xierongchuan@tutamail.com>
2025-08-18 07:02:14 +0500

    Add Start Command Conversation

--- routes/telegram.php
+++ routes/telegram.php
@@ -5,10 +5,24 @@ declare(strict_types=1);
 /** @var SergiX44\Nutgram\Nutgram $bot */
 
 use SergiX44\Nutgram\Nutgram;
-use App\Telegram\Commands\StartCommand;
+use App\Bot\Conversations\Guest\StartCommand;
+use App\Bot\Middleware\AuthUser;
+use App\Bot\Middleware\RoleMiddleware;
+use App\Bot\Dispatchers\StartConversationDispatcher;
+use App\Bot\Dispatchers\ExpenseCommandDispatcher;
 
 /*
 | Nutgram Handlers
 */
 
-$bot->registerCommand(StartCommand::class);
+$bot->onCommand(
+    'start',
+    StartConversationDispatcher::class
+);
+
+// $bot->onCommand(
+//     'expense',
+//     ExpenseCommandDispatcher::class
+// )
+// ->middleware(new RoleMiddleware(['user']))
+// ->middleware(AuthUser::class);
