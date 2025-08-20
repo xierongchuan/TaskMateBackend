ed536ef9fc50421e390ca124aa872f4b489404d8
谢榕川 <xierongchuan@tutamail.com>
2025-08-21 02:34:45 +0500

    Add ExpenseConversation

--- routes/telegram.php
+++ routes/telegram.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 /** @var SergiX44\Nutgram\Nutgram $bot */
 
+use App\Conversations\RequestExpenseConversation;
 use SergiX44\Nutgram\Nutgram;
 use App\Bot\Conversations\Guest\StartCommand;
 use App\Bot\Middleware\AuthUser;
 use App\Bot\Middleware\RoleMiddleware;
 use App\Bot\Dispatchers\StartConversationDispatcher;
-use App\Bot\Dispatchers\ExpenseCommandDispatcher;
 
 /*
 | Nutgram Handlers
@@ -20,9 +20,9 @@ $bot->onCommand(
     StartConversationDispatcher::class
 );
 
-// $bot->onCommand(
-//     'expense',
-//     ExpenseCommandDispatcher::class
-// )
-// ->middleware(new RoleMiddleware(['user']))
-// ->middleware(AuthUser::class);
+$bot->onCommand(
+    'expense',
+    RequestExpenseConversation::class
+)
+->middleware(new RoleMiddleware(['user']))
+->middleware(AuthUser::class);
