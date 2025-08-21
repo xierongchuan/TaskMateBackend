d6307878dc331812ac157c3f20cd924404960e33
谢榕川 <xierongchuan@tutamail.com>
2025-08-21 22:25:22 +0500

    Add comment

--- routes/telegram.php
+++ routes/telegram.php
@@ -4,14 +4,12 @@ declare(strict_types=1);
 
 /** @var SergiX44\Nutgram\Nutgram $bot */
 
-use Illuminate\Support\Facades\Log;
 use SergiX44\Nutgram\Nutgram;
 use App\Bot\Conversations\Guest\StartCommand;
 use App\Bot\Middleware\AuthUser;
 use App\Bot\Middleware\RoleMiddleware;
 use App\Bot\Dispatchers\StartConversationDispatcher;
 use App\Bot\Conversations\User\RequestExpenseConversation;
-use App\Models\User;
 
 /*
 | Nutgram Handlers
