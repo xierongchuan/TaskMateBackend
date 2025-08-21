a23d24604563b2c935d5bd349ddb06948adf5bde
谢榕川 <xierongchuan@tutamail.com>
2025-08-21 22:25:16 +0500

    Add comment

--- routes/telegram.php
+++ routes/telegram.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 /** @var SergiX44\Nutgram\Nutgram $bot */
 
+use Illuminate\Support\Facades\Log;
 use SergiX44\Nutgram\Nutgram;
 use App\Bot\Conversations\Guest\StartCommand;
 use App\Bot\Middleware\AuthUser;
 use App\Bot\Middleware\RoleMiddleware;
 use App\Bot\Dispatchers\StartConversationDispatcher;
 use App\Bot\Conversations\User\RequestExpenseConversation;
+use App\Models\User;
 
 /*
 | Nutgram Handlers
@@ -20,6 +22,7 @@ $bot->onCommand(
     StartConversationDispatcher::class
 );
 
+// Users Middleware
 $bot->onText(
     '📝 Создать заявку',
     RequestExpenseConversation::class
