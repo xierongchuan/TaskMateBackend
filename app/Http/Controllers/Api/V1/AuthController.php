dd3fec572ce8f3c0bccf05239c39405386f81993
谢榕川 <xierongchuan@tutamail.com>
2025-08-27 21:06:17 +0500

    Install Laravel Sanctum

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Enums\Role;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $req)
+    {
+        $req->validate([
+            'login'     => 'required|string|min:4|max:255|unique:users,login',
+            'password'  => 'required|string|min:6|max:255',
+        ]);
+
+        $user = User::create([
+            'login'     => $req->login,
+            'password'  => Hash::make($req->password),
+            'full_name' => '-',
+            'telegram_id' => 0,
+            'company_id' => 0,
+            'phone' => '+0000000000',
+            'role'      => Role::USER->value,
+        ]);
+
+        $token = $user->createToken('user-token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Пользователь успешно зарегистрирован',
+            'token'   => $token,
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,46 @@
 <?php
+
+declare(strict_types=1);
+
+use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\SessionController;
+use App\Http\Controllers\Api\V1\UserApiController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    // Открытие сессии (логин)
+    Route::post(
+        '/session',
+        [SessionController::class, 'store']
+    )->middleware('throttle:5,1440');
+
+    // Регистрация пользовтеля (регистрация)
+    Route::post(
+        '/register',
+        [AuthController::class, 'register']
+    )->middleware('throttle:100,1440');
+
+    // Закрытие сессии (логаут)
+    Route::delete(
+        '/session',
+        [SessionController::class, 'destroy']
+    )->middleware(['auth:sanctum', 'throttle:5,1440']);
+
+    // Проверка работоспособности API
+    Route::get('/up', function () {
+        return response()->json(['success' => true], 200);
+    })->middleware('throttle:100,1');
+
+    // Защищённые маршруты только для админов
+    Route::middleware([
+            'auth:sanctum',      // проверка токена Sanctum
+            'admin.token',       // middleware, сверяющий role === 'admin'
+            'throttle:150,1'     // лимит запросов
+        ])
+        ->group(function () {
+            Route::get('/users', [UserApiController::class, 'index']);
+            Route::get('/users/{id}', [UserApiController::class, 'show']);
+            Route::get('/users/{id}/status', [UserApiController::class, 'status']);
+        });
+});
